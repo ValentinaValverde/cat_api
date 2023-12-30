0e08c1a0a5ae39cd955c00d58539342482f10f2c
Valentina Valverde <vvgreenville@gmail.com>
2023-12-29 23:05:26 -0500

    Added the API and console logged some values, trying to figure out how to display it on the page.

--- src/components/CatAPI.tsx
+++ src/components/CatAPI.tsx
@@ -1,10 +1,35 @@
 import { FC, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 
 const CatAPI: FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [imgUrl, setImgUrl] = useState("");
+
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        "https://api.thecatapi.com/v1/images/search"
+      );
+      console.log(
+        // something to do with types:
+        data.map((data) => {
+          console.log("URL", data.url);
+        })
+      );
+    } catch (error) {
+      console.log("ERROR", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <p> meow cat api page something here ???</p>
+      <p>hello from CAT API</p>
+      <button onClick={handleClick}>Button</button>
+      {loading && <p>Loading...</p>}
+      {/* <img src={data.url} /> */}
     </>
   );
 };
