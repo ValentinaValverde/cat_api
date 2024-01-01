a5fd8a17e16a6b70f6b3a125c3a53011051e775d
Valentina Valverde <vvgreenville@gmail.com>
2024-01-01 13:31:31 -0500

    Created a new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import CatAPI from "./components/CatAPI";
+import SearchCatAPI from "./components/SearchCatAPI";
 
 function App() {
   return (
     <>
       <CatAPI />
+      <SearchCatAPI />
     </>
   );
 }
---
+++ src/components/SearchCatAPI.tsx
@@ -0,0 +1,12 @@
+import { FC, useState } from "react";
+import axios from "axios";
+
+const SearchCatAPI: FC = () => {
+  return (
+    <>
+      <p>Search bar</p>
+    </>
+  );
+};
+
+export default SearchCatAPI;
