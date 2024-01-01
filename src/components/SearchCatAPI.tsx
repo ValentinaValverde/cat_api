8fa8bef951742a115fdb9f817d614f89d1740799
Valentina Valverde <vvgreenville@gmail.com>
2024-01-01 13:36:36 -0500

    Added a search bar

--- src/components/SearchCatAPI.tsx
+++ src/components/SearchCatAPI.tsx
@@ -4,9 +4,20 @@ import axios from "axios";
 const SearchCatAPI: FC = () => {
   return (
     <>
-      <p>Search bar</p>
+      <div>
+        <h1>Looking for a Photo?</h1>
+        <label>
+          Type ID Here: &nbsp;
+          <input type="text" />
+        </label>
+      </div>
     </>
   );
 };
 
 export default SearchCatAPI;
+
+// GOAL:
+// When the user types in the id into the search bar,
+// that id is put into the API url and it retrieves the
+// photo that matches the id
